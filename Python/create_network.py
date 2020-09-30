be72b937e71dcaaba03455a9afec1a124a451535
Mohammad Rahman <mbrahman@gmail.com>
2020-09-30 07:54:40 -0400

    create network script

--- Python/create_network.py
+++ Python/create_network.py
@@ -7,7 +7,10 @@ def GenerateConfig(context):
         'name': 'vpc-1',
         'type': 'compute.v1.network',
         'properties': {
-            
+            "autoCreateSubnetworks": False,
+            "routingConfig": {
+                "routingMode": "REGIONAL"
+            }
         }
     })
 
